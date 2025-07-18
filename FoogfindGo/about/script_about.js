f8ee6daef17e5de0e98298fabb1bb1b31b8d515e
Arits <151486530+Aritssara@users.noreply.github.com>
2025-07-18 20:59:26 +0700

    Delete FoogfindGo directory

--- FoogfindGo/about/script_about.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleBtn = document.getElementById('menu-toggle');
-    const menu = document.getElementById('menu');
-
-    toggleBtn.addEventListener('click', function() {
-        menu.classList.toggle('show');
-    });
-});
